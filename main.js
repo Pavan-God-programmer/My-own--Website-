bfea798e4ef569708d1448fdf418c0b5256c71e0
Pavan-God-programmer <p.thamaraikannan@share.epsb.ca>
2022-09-18 17:39:32 -0600

    OKay

--- main.js
+++ main.js
@@ -3,14 +3,20 @@
 // IPO Algorithm (Input-Process-Output)
 
 //Input
-let like = prompt("Do you like games?");
-let toppings = prompt("In what divice you would like to play?");
+let username = prompt("What is your name ");
+let like = prompt("Do you like Video Games?");
+let divice = prompt("In what divice you would like to play?");
 
 //Process
-let msg = `Do you like games: ${like}. In what divice you would like to play: ${toppings}`; 
- 
-let mg = "Let me present you War Robots";
+let meg = `WOW, ${username} You Do Like Video Games`;
+let msg = `Awasome, Nice to Hear ${username}`; 
+let mg = "Let me present you The War Robots";
+let Like= `WOW,${username} You Like to play in ${divice}`;
+let present= `OKAY, THEN`
 
 //Output
-alert(msg);
+alert(Like)
+alert(meg);
+alert(msg); 
+alert(present)
 alert(mg);
