c40bd4cdb5e12b3288c1e65521c2f80dba264f7c
Pavan-God-programmer <p.thamaraikannan@share.epsb.ca>
2022-09-18 00:01:53 -0600

    trying java

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 // JavaScript Code
+
+// IPO Algorithm (Input-Process-Output)
+
+//Input
+let like = prompt("Do you like games?");
+let toppings = prompt("In what divice you would like to play?");
+
+//Process
+let msg = `Do you like games: ${like}. In what divice you would like to play: ${toppings}`; 
+ 
+let mg = "Let me present you War Robots";
+
+//Output
+alert(msg);
+alert(mg);
