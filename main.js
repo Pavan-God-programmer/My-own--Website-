c0d10fdb902a286b23647b50ab8f80dbb54103cf
Pavan-God-programmer <p.thamaraikannan@share.epsb.ca>
2022-09-23 11:20:38 -0600

    wowwwwwww

--- main.js
+++ main.js
@@ -1,22 +1,17 @@
 // JavaScript Code
 
+const username = localStorage.getItem("username");
+
+if (!username) {
+  let username = prompt("What is your name ") || "noneSet";
+  let like = prompt("Do you like Video Games?");
+  let divice = prompt("In what divice you would like to play?");
+
+  console.log(username);
+  localStorage.setItem("username", username);
+}
+
+if (username !== "noneSet" && username !== null) {
+  document.getElementById("username").innerHTML = `${username}`;
+}
 // IPO Algorithm (Input-Process-Output)
-
-//Input
-let username = prompt("What is your name ");
-let like = prompt("Do you like Video Games?");
-let divice = prompt("In what divice you would like to play?");
-
-//Process
-let meg = `WOW, ${username} You Do Like Video Games`;
-let msg = `Awasome, Nice to Hear ${username}`; 
-let mg = "Let me present you The War Robots";
-let Like= `WOW,${username} You Like to play in ${divice}`;
-let present= `OKAY, THEN`
-
-//Output
-alert(Like)
-alert(meg);
-alert(msg); 
-alert(present)
-alert(mg);
--- main2.js
+++ main2.js
@@ -1,1 +1,26 @@
 // JavaScript Code
+
+//input
+//Process
+const button = document.getElementById("btn1");
+
+button.addEventListener("click", () => {
+  const q1 = document.getElementById("q1").value;
+  const q2 = document.getElementById("q2").value;
+  const q3 = document.getElementById("q3").value;
+  const q4 = document.getElementById("q4").value;
+  const q5 = document.getElementById("q5").value;
+  const q6 = document.getElementById("q6").value;
+
+  console.log(q1);
+  if (!q1) {
+    return (document.getElementById("q1res").innerHTML = "❌ Missing");
+  }
+
+  if (q1 === "multiplayer" || q1 === "answer2") {
+    document.getElementById("q1res").innerHTML = "✅ correct";
+  } else {
+    document.getElementById("q1res").innerHTML = "❌ Wrong";
+  }
+});
+//Output
