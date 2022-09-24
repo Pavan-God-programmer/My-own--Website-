73ac6f05a21175b06fcd75e994c8b6ae7035158e
Pavan-God-programmer <p.thamaraikannan@share.epsb.ca>
2022-09-23 22:06:19 -0600

    i am scruder

--- main2.js
+++ main2.js
@@ -1,26 +1,21 @@
 // JavaScript Code
 
-//input
-//Process
-const button = document.getElementById("btn1");
+//Event Listerner on the button 
+document.getElementById("btn").addEventListener("click", btnClicked);
 
-button.addEventListener("click", () => {
-  const q1 = document.getElementById("q1").value;
-  const q2 = document.getElementById("q2").value;
-  const q3 = document.getElementById("q3").value;
-  const q4 = document.getElementById("q4").value;
-  const q5 = document.getElementById("q5").value;
-  const q6 = document.getElementById("q6").value;
 
-  console.log(q1);
-  if (!q1) {
-    return (document.getElementById("q1res").innerHTML = "❌ Missing");
-  }
+function btnClicked() {
+   //input
+  let q2 = +document.getElementById("q2").value;
+  let q3 = +document.getElementById("q3").value;
+  let q4 = +document.getElementById("q4").value;
+  let q5 = +document.getElementById("q5").value;
+  let q6 = +document.getElementById("q6").value;
+ 
+  // Process
+  let total = (q2 + q3 + q4 + q5 + q6);
 
-  if (q1 === "multiplayer" || q1 === "answer2") {
-    document.getElementById("q1res").innerHTML = "✅ correct";
-  } else {
-    document.getElementById("q1res").innerHTML = "❌ Wrong";
-  }
-});
-//Output
+  // Output
+  document.getElementById("output").innerHTML = total;
+  
+}
