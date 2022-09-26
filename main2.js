13d1806c2e4079da5d6f5b1af95d39637f91ba75
Pavan-God-programmer <p.thamaraikannan@share.epsb.ca>
2022-09-26 10:40:52 -0600

    shit

--- main2.js
+++ main2.js
@@ -1,29 +1,9 @@
 // JavaScript Code
 
-//Event Listerner on the button 
-document.getElementById("btn").addEventListener("click", btnClicked);
+//Event Listerner on the button
 
+//input
 
-function btnClicked() {
-   //input
-  let q2 = +document.getElementById("q2").value;
-  let q3 = +document.getElementById("q3").value;
-  let q4 = +document.getElementById("q4").value;
-  let q5 = +document.getElementById("q5").value;
-  let q6 = +document.getElementById("q6").value;
- 
-  // Process
-  let q2s = (q2);
-  let q3s = (q3);
-  let q4s = (q4);
-  let q5s = (q5);
-  let q6s = (q6);
+// Processs
 
-
-  // Output
-  document.getElementById("q2s").innerHTML = q2s;
-  document.getElementById("q3s").innerHTML = q3s;
-  document.getElementById("q4s").innerHTML = q4s;
-  document.getElementById("q5s").innerHTML = q5s;
-  document.getElementById("q6s").innerHTML = q6s;  
-}
+// Output
