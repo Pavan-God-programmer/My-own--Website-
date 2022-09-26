be71008a0db8c1cdeeab05a96f4a91c43215cc5b
Pavan-God-programmer <p.thamaraikannan@share.epsb.ca>
2022-09-25 20:44:00 -0600

    IdontKnpw

--- main2.js
+++ main2.js
@@ -13,9 +13,17 @@ function btnClicked() {
   let q6 = +document.getElementById("q6").value;
  
   // Process
-  let total = (q2 + q3 + q4 + q5 + q6);
+  let q2s = (q2);
+  let q3s = (q3);
+  let q4s = (q4);
+  let q5s = (q5);
+  let q6s = (q6);
+
 
   // Output
-  document.getElementById("output").innerHTML = total;
-  
+  document.getElementById("q2s").innerHTML = q2s;
+  document.getElementById("q3s").innerHTML = q3s;
+  document.getElementById("q4s").innerHTML = q4s;
+  document.getElementById("q5s").innerHTML = q5s;
+  document.getElementById("q6s").innerHTML = q6s;  
 }
