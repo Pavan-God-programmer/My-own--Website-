92cd0dc799b9ab4c337ac454acb78b47b28ec267
Pavan-God-programmer <p.thamaraikannan@share.epsb.ca>
2022-10-05 10:35:33 -0600

    woowooowowoow

--- main2.js
+++ main2.js
@@ -1,9 +1,15 @@
 // JavaScript Code
 
 //Event Listerner on the button
+document.getElementById("btn").addEventListener("click", btnClicked);
 
-//input
+function btnClicked() {
+  const q1value = document.getElementById("q1input").value.toLowerCase();
+  console.log(q1value);
 
-// Processs
-
-// Output
+  if (q1value === "phone") {
+    document.getElementById("q1output").innerHTML = "Right";
+  } else {
+    document.getElementById("q1output").innerHTML = "Wrong";
+  }
+}
